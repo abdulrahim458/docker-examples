d645d15beced15ed80ae67ecc5f4a4a1639ddff5
abdulrahim458 <67860725+abdulrahim458@users.noreply.github.com>
2022-09-17 23:51:27 +0530

    feat: Script to generate inputfie with random no's

---
+++ solution/gencsv.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+if [[ ! -e ./inputFile ]]; then
+    touch ./inputFile
+fi
+
+RANDOM=$$
+
+num=0
+
+while [[ $[num] -le 9 || $[num] -le $1 ]]
+do
+   echo $num, $RANDOM >> inputFile
+
+(( num = num + 1 ))
+done
